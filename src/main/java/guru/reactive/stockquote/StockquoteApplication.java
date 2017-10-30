7f3cca5c20c201d26cea32dbf6923ad19122375a
bipin <bipinbipin@gmail.com>
2017-10-30 15:46:53 -0500

    Initial commit

---
+++ src/main/java/guru/reactive/stockquote/StockquoteApplication.java
@@ -0,0 +1,12 @@
+package guru.reactive.stockquote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockquoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockquoteApplication.class, args);
+	}
+}
---
+++ src/main/java/guru/reactive/stockquote/services/QuoteGeneratorService.java
@@ -0,0 +1,11 @@
+package guru.reactive.stockquote.services;
+
+import guru.reactive.stockquote.model.Quote;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+public interface QuoteGeneratorService {
+
+    Flux<Quote> fetchQuoteStream(Duration period);
+}
